a3bc154e049a947f37ea387b8a17d94879708649
deiken21 <ajanth21@live.co.uk>
2021-10-28 14:32:57 +0100

    changed to classes

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
-"use strict"
-
 let dogs=[]
 
-dogs.push({name:"Fluffy",Breed:"Poodle",picture:"/Images/",age:5})
-dogs.push({name:"Otto",Breed:"Chiwowa",picture:"/Images/",age:7})
-dogs.push({name:"Alfie",Breed:"French Bulldog",picture:"/Images/",age:4})
-dogs.push({name:"Lucifer",Breed:"Siberian husky",picture:"/Images/",age:7})
-dogs.push({name:"Stella",Breed:"Golden retreiver",picture:"/Images/",age:4})
-dogs.push({name:"Sora",Breed:"Japanese akita",picture:"/Images/",age:8})
+class Dog{
+    constructor(name,Breed,picture,age){
+        this.name=name;
+        this.Breed=Breed
+        this.picture=picture
+        this.age=age
+    }
+}
+function setupDogs(){
+    dogs.push(new Dog("Fluffy","Poodle","/Images/","5 yr"))
+    dogs.push(new Dog("Otto","Chiwowa","/Images/","7 yr"))
+    dogs.push(new Dog("Alfie","French Bulldog","/Images/","4 yr"))
+    dogs.push(new Dog("Lucifer","Siberian husky","/Images/","7yr"))
+    dogs.push(new Dog("Stella","Golden retreiver","/Images/","4 yr"))
+    dogs.push(new Dog("Sora","Japanese akita","/Images/","8 yr"))
+}
 
+function dogdetail(){
+    for(let i=0;i<dogs.length;i++){
 
-//6 dogs - on 300*400 pixel cards (with nice padding and margins, and a nice background colour)
-// An image for each dog
-// Display Breed
-// Add an age property for every dog
-// add an 'Adopt Now' button on each dog
-
-for(let i=0;i<dogs.length;i++){
-
-    let card = document.createElement("div")
-    card.classList.add("card")
-    document.getElementById("cardholder").appendChild(card)
+        let card = document.createElement("div")
+        card.classList.add("card")
+        document.getElementById("cardholder").appendChild(card)
 
     //create dogname
-    let dogName=document.createElement("h1")
-    card.appendChild(dogName)
-    dogName.innerHTML=dogs[i].name
+        let dogName=document.createElement("h1")
+        card.appendChild(dogName)
+        dogName.innerHTML=dogs[i].name
 
-    let dogBreed=document.createElement("h2")
-    card.appendChild(dogBreed)
-    dogBreed.innerHTML=dogs[i].Breed
+        let dogBreed=document.createElement("h2")
+        card.appendChild(dogBreed)
+        dogBreed.innerHTML=dogs[i].Breed
+    
+        let dogAge=document.createElement("h2")
+        card.appendChild(dogAge)
+        dogAge.innerHTML=dogs[i].age
 
-    let dogPicture=document.createElement("img")
-    dogPicture.classList.add("dogpicture")
-    card.appendChild(dogPicture)
-    dogPicture.src=dogs[i].picture+dogs[i].name +".jpg" // dogPicture.src=/Images/Fluffy.jpg
+        let dogPicture=document.createElement("img")
+        dogPicture.classList.add("dogpicture")
+        card.appendChild(dogPicture)
+        dogPicture.src=dogs[i].picture+dogs[i].name+".jpg" // dogPicture.src=/Images/Fluffy.jpg
 
-    let adoptNow=document.createElement("button")
-    adoptNow.classList.add("button")
-    adoptNow.innerHTML="Adopt Now"
-    card.appendChild(adoptNow)
+        let adoptNow=document.createElement("button")
+        adoptNow.classList.add("button")
+        adoptNow.innerHTML="Adopt Now"
+        card.appendChild(adoptNow)
+
 
 
 }
+}
 
+setupDogs()
+
+dogdetail()
